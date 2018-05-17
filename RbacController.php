928734646e103e9158ef5a89aeffc5398db68037
karthus <1426921812@qq.com>
2018-05-18 01:04:10 +0800

    RBAC 2

--- RbacController.php
+++ RbacController.php
@@ -8,9 +8,13 @@ class RbacController extends Controller
 {
     public function actionInit()
     {
-        $auth = Yii::$app->authManager;
-        $auth->removeAll();
+        $auth = Yii::$app->authManager; // authManager组件
+        // $auth->removeAll();
 
+
+        /**
+        * createPermissionz这个方法，相当于在auth_item表中增加一条权限记录
+        */
         // 添加 "createPost" 权限
         $createPost = $auth->createPermission('createPost');
         $createPost->description = 'Create a post';
@@ -21,6 +25,12 @@ class RbacController extends Controller
         $updatePost->description = 'Update post';
         $auth->add($updatePost);
 
+
+
+        /**
+        * createRole这个方法，相当于在auth_item表中增加一条角色记录
+        * addChild这个方法，相当于在auth_item_child表中添加一条建立关系的记录
+        */
         // 添加 "author" 角色并赋予 "createPost" 权限
         $author = $auth->createRole('author');
         $auth->add($author);
@@ -33,6 +43,11 @@ class RbacController extends Controller
         $auth->addChild($admin, $updatePost);
         $auth->addChild($admin, $author);
 
+
+
+        /**
+        * assign这个方法，是给用户分配角色，相当于在auth_assignment表中添加一条记录
+        */
         // 为用户指派角色。其中 1 和 2 是由 IdentityInterface::getId() 返回的id
         // 通常在你的 User 模型中实现这个函数。
         $auth->assign($author, 2);
